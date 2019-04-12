0c59f11aac17ee985655780227119bab0e591be2
Monica Powell <monica@aboutmonica.com>
2019-04-11 20:40:26 -0400

    add a template for generating the metadata for Gatsby blog post

--- plopfile.js
+++ plopfile.js
@@ -1,7 +1,38 @@
 module.exports = function (plop) {
+    plop.setGenerator('Gatsby Blog Post', {
+        description: 'Templates for Gatsby templates',
+        prompts: [{
+            type: 'input',
+            name: 'title',
+            message: 'Title of post:'
+        }, {
+            type: 'input',
+            name: 'description',
+            message: 'Description of post:'
+        },
+    
+        {
+            type: 'list',
+            name: 'category',
+            message: 'Category:',
+            choices: ['Tutorial', 'TIL', 'Narrative', 'Reflection'],
+            filter: function(val) {
+              return val.toLowerCase();
+            },
+        }
+        // { name: 'date',
+        // return new  Date();}
+        
+        ],
+        actions: [{
+            type: 'add',
+            path: 'src/content/{{dashCase title}}.md',
+            templateFile: 'src/plop-templates/blog-post.hbs'
+        } ]
+    });
   
     plop.setGenerator('stateless component', {
-        description: 'application controller logic',
+        description: 'template for stateless components',
         prompts: [{
             type: 'input',
             name: 'name',
